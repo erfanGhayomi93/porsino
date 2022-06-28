33c71e9a2b54a5adb587af12d8f33ced9c630521
erfanGhayomi93 <erfuunghayomi@gmail.com>
2022-06-28 13:20:40 +0430

    handle competition ui

--- modules/create/release/presentation/components/Targeted/Targeted.controller.tsx
+++ modules/create/release/presentation/components/Targeted/Targeted.controller.tsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 export type TargetedProps = {};
 
 export default function useTargetedController(props: TargetedProps) {
-  const [isShowTypeQues, setisShowTypeQues] = useState(true);
+  const [isShowTypeQues, setisShowTypeQues] = useState(false);
   const {} = props;
   return { ...props, isShowTypeQues, setisShowTypeQues };
 }
