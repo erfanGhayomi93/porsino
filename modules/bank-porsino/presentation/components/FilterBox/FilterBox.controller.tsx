642e2e2d136b46a6e98182578ed88a41e3ba4311
erfanGhayomi93 <erfuunghayomi@gmail.com>
2022-06-29 14:33:42 +0430

    handle chips bankPorsino

--- modules/bank-porsino/presentation/components/FilterBox/FilterBox.controller.tsx
+++ modules/bank-porsino/presentation/components/FilterBox/FilterBox.controller.tsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 export type FilterBoxProps = {};
 
 export default function useFilterBoxController(props: FilterBoxProps) {
-  const [isShowFilter, setisShowFilter] = useState(true);
+  const [isShowFilter, setisShowFilter] = useState(false);
   const {} = props;
   return { ...props, isShowFilter, setisShowFilter };
 }
--- modules/bank-porsino/presentation/pages/BankPorsino/index.tsx
+++ modules/bank-porsino/presentation/pages/BankPorsino/index.tsx
@@ -1,30 +1,52 @@
 import AppHeaderComponent from "@/core/components/layouts/AppHeader";
 import NavigationBottomComponent from "@/core/components/layouts/NavigationBottom";
-import { Box, Container } from "@mui/material";
+import { Box, Chip, Container } from "@mui/material";
 import CardBankComponent from "../../components/CardBank";
 import FilterBoxComponent from "../../components/FilterBox";
 import useBankPorsinoController, {
   BankPorsinoProps,
 } from "./BankPorsino.controller";
 import styles from "./BankPorsino.module.scss";
+import { RiCloseLine } from "react-icons/ri";
 
 export default function BankPorsinoPage(props: BankPorsinoProps) {
   const {} = useBankPorsinoController(props);
+
+  const handleDelete = () => {};
 
   return (
     <NavigationBottomComponent value={3}>
       <AppHeaderComponent title="بانک پرسینو" />
       <Container maxWidth="lg">
-        <FilterBoxComponent />
+        <div className={styles.root}>
+          <FilterBoxComponent />
 
-        <Box className={styles.cards}>
-          <CardBankComponent />
-          <CardBankComponent />
-          <CardBankComponent />
-          <CardBankComponent />
-          <CardBankComponent />
-        </Box>
+          {true && (
+            <div className={styles.chips}>
+              {dataChips.map((item, ind) => (
+                <Chip
+                  key={ind}
+                  label={item.label}
+                  className={styles.chip}
+                  clickable
+                  deleteIcon={<RiCloseLine className={styles.deleteIcon} />}
+                  onDelete={handleDelete}
+                />
+              ))}
+            </div>
+          )}
+
+          <Box className={styles.cards}>
+            <CardBankComponent />
+            <CardBankComponent />
+            <CardBankComponent />
+            <CardBankComponent />
+            <CardBankComponent />
+          </Box>
+        </div>
       </Container>
     </NavigationBottomComponent>
   );
 }
+
+const dataChips = [{ label: "آزمون" }, { label: "روان‌شناسی" }];
